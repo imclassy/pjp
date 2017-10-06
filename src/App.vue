299569d77fd40eb003161bd3fdeb4c9211f5f6e4
imclassy <pe.jaraba@hotmail.com>
2017-10-06 17:21:23 -0500

    Se arreglo imagen de fondo de pagina de inicio

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" >
     <router-view></router-view>
   </div>
 </template>
@@ -15,8 +15,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
 }
 </style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="container">
-    <div class="hello col-md-6 col-md-offset-3">
-      <h1>{{ msg }}</h1>
-      </br>
-      <p id="intro">
-        {{intro1}}
-        </br></br>
-        {{intro2}}
-      </p>
-      </br>
-      <img src="../assets/profile_pic_1.png">
-      <hr>
-      <ul>
-        <li><a href="http://router.vuejs.org/" target="_blank">About</a></li>
-        <li><a href="http://vuex.vuejs.org/" target="_blank">Skills</a></li>
-        <li><a href="http://vue-loader.vuejs.org/" target="_blank">Portfolio</a></li>
-        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">Contact</a></li>
-      </ul>
+  <div class="container-fluid">
+    <div class="bg-image"></div>
+    <div class="content">
+      <div class="hello col-md-6 col-md-offset-3">
+        <h1>{{ msg }}</h1>
+        </br>
+        <p id="intro">
+          {{intro1}}
+          </br></br>
+          {{intro2}}
+        </p>
+        </br>
+        <img src="../assets/profile_pic_1.png">
+        <hr>
+        <ul>
+          <li><a href="http://router.vuejs.org/" target="_blank">About</a></li>
+          <li><a href="http://vuex.vuejs.org/" target="_blank">Skills</a></li>
+          <li><a href="http://vue-loader.vuejs.org/" target="_blank">Portfolio</a></li>
+          <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">Contact</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -36,6 +39,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+body{
+  background:url("../assets/big-data.jpg");
+}
 
 h1{
   font-family: "brandon-grotesque", sans-serif !important;
@@ -51,10 +57,25 @@ li {
 }
 
 a {
-  color: #42b983;
+  color: #F5F5F5;
 }
 
 p{
   text-align: justify
+}
+.bg-image{
+  background-image:url("../assets/big-data.jpg");
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+.hello{
+  margin-top: 60px;
+  text-align: center;
+  color: #FFFFFF;
+  background-color: rgba(0,0,0, 0.6);
 }
 </style>
