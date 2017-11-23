4c907ded790aa8085412512667add803cc152a02
imclassy <pe.jaraba@hotmail.com>
2017-11-23 17:28:07 -0500

    Started Donut component

--- src/components/portfolio/graphs/DeathCountBySex.vue
+++ src/components/portfolio/graphs/DeathCountBySex.vue
@@ -16,27 +16,41 @@ export default {
   },
   mounted () {
     d3.json('/static/json/death_count_grouped_by_sex.json', function (data) {
+      var colors = {}
+      colors['MALE'] = '#247BA0'
+      colors['FEMALE'] = '#FF1654'
+      colors['UNKNOWN'] = '#F3FFBD'
+
+      var coloredData = []
+      for (var i = 0; i < data.length; i++) {
+        var coloredRecord = data[i]
+        coloredRecord.color = colors[coloredRecord.value.name]
+        coloredData.push(coloredRecord)
+      }
       nv.addGraph(function () {
         var chart = nv.models.pieChart()
             .x(function (d) { return d.value.name })
             .y(function (d) { return d.frequency })
             .showLabels(true)     // Display pie labels
-            .labelThreshold(0.05)  // Configure the minimum slice size for labels to show up
+            // .labelThreshold(0.05)  // Configure the minimum slice size for labels to show up
             .labelType('percent') // Configure what type of data to show in the label. Can be "key", "value" or "percent"
             .donut(true)          // Turn on Donut mode. Makes pie chart look tasty!
             .donutRatio(0.35)     // Configure how big you want the donut hole size to be.
             .width(500)
             .height(350)
+            .color(function (d) { return d.color })
+            .id('piechart')
 
         d3.select('#graph')
             .append('svg')
-            .datum(data)
+            .datum(coloredData)
             .transition().duration(350)
             .call(chart)
 
         d3.selectAll('.nv-pieLabels text').style('fill', 'white').style('font', 'Roboto')
         d3.selectAll('.nv-legend-text').style('fill', 'white').style('font', 'Roboto')
         d3.selectAll('.nvd3 text').style('fill', 'white').style('font', 'Roboto')
+        d3.selectAll('.nvd3.nv-pie path').style('stroke-opacity', '0')
         return chart
       })
     })
@@ -53,4 +67,5 @@ export default {
   .nvd3 text {
    font: normal 12px Roboto !important;
   }
+
 </style>
